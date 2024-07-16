1ae668c1338f8bb65522804d55f4671d2bd695d4
Charles042 <ekechukwuma20@gmail.com>
2024-07-16 05:31:14 +0100

    finished with submit functionality

--- src/components/Newpost.jsx
+++ src/components/Newpost.jsx
@@ -2,21 +2,40 @@ import { useState } from "react";
 import styles from "./NewPost.module.css";
 
 
-function Newpost({change,author,cancel}) {
+function Newpost({onCancel}) {
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  function changeHandler(event) {
+    setText(event.target.value);
+  }
+  function changeAuthor(event) {
+    setAuthor(event.target.value);
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    const postData = {
+      body : text,
+      author: author
+    } 
+    console.log(postData)
+    onCancel();
+  }
   return (
   
-      <form className={styles.form}>
+      <form className={styles.form}  onSubmit={handleSubmit}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={change} />
+          <textarea id="body" required rows={3} onChange={changeHandler} />
         </p>
         <p>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" required onChange={author} />
+          <input type="text" id="name" required onChange={changeAuthor} />
         </p>
 
         <p className = {styles.actions}>
-          <button onClick={cancel}>Cancel</button>
+          <button onClick={onCancel}>Cancel</button>
           <button>Submit</button>
         </p>
       </form>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,24 +5,15 @@ import Modal from "./Modal";
 import styles from "./PostList.module.css";
 
 function PostList({isPosting, handleVisble}) {
-  const [text, setText] = useState("");
-  const [author, setAuthor] = useState("");
-
-  function changeHandler(event) {
-    setText(event.target.value);
-  }
-  function changeAuthor(event) {
-    setAuthor(event.target.value);
-  }
+  
   return (
     <>
       {isPosting && (
         <Modal onClose={handleVisble}>
-          <Newpost change={changeHandler} author={changeAuthor} cancel={handleVisble}/>
+          <Newpost onCancel={handleVisble}/>
         </Modal>
       )}
       <ul className={styles.posts}>
-        <Post author={author} body={text} />
         <Post author="Charles ginger" body="Love is nothing" />
       </ul>
     </>
